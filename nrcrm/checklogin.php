20700e88d96cf489a1646d7031c73b2a5ccbb5f8
nino.roldan <nino_roldan@yahoo.com>
2014-06-09 17:36:29 +0900

    initial commit

---
+++ nrcrm/checklogin.php
@@ -0,0 +1,34 @@
+<?php
+	include('inc/dbhelper.php');
+	$urlinvaliduserpwd = "Location:login.php?problem=invalidUserPwd";
+	session_start();
+
+	
+	//	Validation block
+	$username = cleaninput($_POST['username']);
+	$password = cleaninput($_POST['nrpassword']);
+		
+	connectToDbase('nrcrm');
+	
+	//prepare query string to find out if the username and password is present in the database
+	
+	$checkstring="SELECT * from tblusers WHERE username = '".$username."' AND password = '".$password."'";
+    
+    $result=mysql_query($checkstring);
+    
+    $num_result=mysql_num_rows($result);
+
+    if ($num_result == 0){
+						header($urlinvaliduserpwd);
+						exit;      
+						 } else {
+							$_SESSION['name'] = $username; 
+							$_SESSION['loggedin'] = "Yes";
+							$urlcorrect = "Location:landing.php";
+							header($urlcorrect);
+							exit;
+						}
+	
+?>
+
+
