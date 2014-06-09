79dc4037ffe6acedc6d3bd7df895e45289c810a3
nino.roldan <nino_roldan@yahoo.com>
2014-06-09 17:36:40 +0900

    initial commit

---
+++ nrcrm/clientvisitentry.php
@@ -0,0 +1,69 @@
+<html>
+<head meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title></title>
+</head>
+<?php
+	session_start();
+	//echo $_SESSION['name'];
+		
+	include('inc/sessionvalidation.php');
+
+	//GET to get the ClientID
+	$clientID = $_GET['id'];
+?>
+
+<!-- form section starts here-->
+<body>
+<!--?php echo session_id() ;?--> 
+<center>
+<h1>Client Visit Entry Form</h1>
+<form method="POST" action="clientvisitentry_process.php?id=<?php echo $clientID; ?>">					
+		
+		<table border="2">
+			<tr>
+			<th>
+			<label for="visitdate">Date</label>
+			</th>
+			<td>
+			<input type="text" name="visitdate" id="visitdate">
+			</td>
+			</tr>
+					
+			<tr>
+			<th>
+			<label for="visittime">Time</label>
+			</th>
+			<td>
+			<input type="text" name="visittime" id="visittime">
+			</td>
+			</tr>
+					
+			<tr>
+			<th>
+			<label for="visitlocation">Location</label>
+			</th>
+			<td>
+			<input type="text" name="visitlocation" id="visitlocation">
+			</td>
+			</tr>		
+					
+					
+					
+			<tr>
+			<th>
+			<label for="visitnotes">Visit Notes</label>
+			</th>
+			<td>
+			<textarea name="visitnotes" id="visitnotes"></textarea>
+			</td>
+			</tr>
+		</table>	
+			<br>
+			
+			<input type="hidden" name="interactionby" value="<?php echo $username; ?>">
+			<input type="submit" value="Submit">				
+			</center>
+</form>
+
+</body>
+</html>
