b36ea777398c2619b12e24686e36624c5dc69eca
nino.roldan <nino_roldan@yahoo.com>
2014-06-10 10:54:16 +0900

    implemented css

--- nrcrm/newcliententry_process.php
+++ nrcrm/newcliententry_process.php
@@ -1,3 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href='http://fonts.googleapis.com/css?family=Changa+One|Open+Sans:400italic,700italic,400,700,800' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/main.css" >
+    <link rel="stylesheet" href="css/responsive.css" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nenu Roldan | Designer</title>
+  </head>
+  <body>
+    <header>
+     <a href="login.php.html" id="logo">
+           <h1>Client Interaction Management</h1>  
+		<h2>NTMC Foundation</h2>
+      </a>
+      <nav>
+        <ul>
+         <li><a href="newcliententry.php">New Client</a></li>
+          <li><a href="clientvisitentry.php" class="selected">New Interaction</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+        
+      </nav>
+   
+    </header>
+
+
 <?php
 	//session validation process 
 	session_start();
@@ -36,3 +65,5 @@
 		}
 
 ?>
+</body>
+</html>
