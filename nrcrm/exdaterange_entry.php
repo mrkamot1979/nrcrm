91162b8b011bd178b7f6b18f38b59b492b19d5a3
nino.roldan <nino_roldan@yahoo.com>
2014-06-13 17:36:09 +0900

    INITIAL COMMIT

---
+++ nrcrm/exdaterange_entry.php
@@ -0,0 +1,39 @@
+<?php
+  session_start();
+  $pagetitle = "Extract by Date";
+?>
+<html>
+<?php
+  include('inc/header.php');
+  include('inc/sessionvalidation.php');
+?>
+<h1>Enter Date Range</h1>
+<h2>Date format is YYYY/MM/DD</h2>
+<br>
+<center>
+  <form name="exdaterange" method="POST" action="#">
+    <table>
+      <tr>
+        <th><label for="frmdate">From Date:</label></th>
+        <td><input type="text" name="frmdate" id="frmdate"></td>
+      </tr>
+
+      <tr>
+        <th><label for="todate">To Date:</label></th>
+        <td><input type="text" name="todate" id="todate"></td>
+      </tr>
+
+
+
+      </table>
+    <br>
+    <input type="submit" value="Submit" />
+  </form>
+</center>
+
+
+
+
+
+</body>
+</html>
