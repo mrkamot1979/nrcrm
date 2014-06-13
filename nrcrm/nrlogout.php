d1bab9bf027fe2351cd863e4876e30cd00b23af4
nino.roldan <nino_roldan@yahoo.com>
2014-06-13 17:33:30 +0900

    updated

--- nrcrm/nrlogout.php
+++ nrcrm/nrlogout.php
@@ -1,15 +1,24 @@
-<?php 
+<?php
  // you have to open the session first 
- session_start(); 
+ session_start();
  
  //remove all the variables in the session 
- session_unset(); 
- 
- // destroy the session 
- $logoutconfirm = session_destroy();  
- 
+ session_unset();
+ // destroy the session
+ $logoutconfirm = session_destroy();
+
  if ($logoutconfirm) {
 		echo "Logout confirmed!";
-		echo $_SESSION['name'];
 	}
- ?> 
+ ?>
+
+ <html>
+ <head><title>Logout page</title></head>
+ <body>
+ <p>Go back to <a href="login.php">login</a> page.</p>
+
+
+ </body>
+
+
+ </html>
