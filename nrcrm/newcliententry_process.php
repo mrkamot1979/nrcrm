c12ba2af302f5782824d577fd5aaa114263d3ed0
nino.roldan <nino_roldan@yahoo.com>
2014-06-13 11:01:17 +0900

    added inc/header

--- nrcrm/newcliententry_process.php
+++ nrcrm/newcliententry_process.php
@@ -1,40 +1,17 @@
 <!DOCTYPE html>
+<?php
+session_start();
+$pagetitle = "New Client Entry";
+?>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link href='http://fonts.googleapis.com/css?family=Changa+One|Open+Sans:400italic,700italic,400,700,800' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/main.css" >
-    <link rel="stylesheet" href="css/responsive.css" >
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nenu Roldan | Designer</title>
-  </head>
-  <body>
-    <header>
-     <a href="landing.php.html" id="logo">
-           <h1>Client Interaction Management</h1>  
-		<h2>NTMC Foundation</h2>
-      </a>
-      <nav>
-        <ul>
-         <li><a href="newcliententry.php">New Client</a></li>
-          <li><a href="newinteraction_search.php" class="selected">New Interaction</a></li>
-          <li><a href="contact.html">Contact</a></li>
-        </ul>
-        
-      </nav>
-   
-    </header>
+<?php
+include('inc/header.php');
+include('inc/sessionvalidation.php');
+include('inc/dbhelper.php');
+?>
 
 
 <?php
-	//session validation process 
-	session_start();
-	//session validation script
-	include('inc/sessionvalidation.php');
-	//dbhelper for mysql connection
-	include('inc/dbhelper.php');
-	
 //portion where the variables are validated and entered onto mysql using $_POST
 	$procfname = cleaninput($_POST['fname']);
 	$proclname = cleaninput($_POST['lname']);
@@ -43,15 +20,15 @@
 	$procemail = cleaninput($_POST['email']);
 	$proccompname = cleaninput($_POST['compname']);
 	$procusername = cleaninput($_POST['enteredby']);
-	
+
 	//function to connect to database
 	connectToDbase('nrcrm');
-	
+
 	//prepare the insert string
 	$insertdata = "INSERT INTO tblclientdetails (ClientID, fname, lname, address, gender, email, company, enteredby) VALUES (NULL, '".$procfname."', '".$proclname."', '".$procaddress."', '".$procgender."', '".$procemail."', '".$proccompname."', '".$procusername."')";
-	
+
 	$result=mysql_query($insertdata);
-	
+
 	if ($result)
 		{
 			echo mysql_affected_rows()." client names entered";
