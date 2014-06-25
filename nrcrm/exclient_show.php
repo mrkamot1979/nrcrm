77f4c47f9e1f7ab285c312bc8b4803c2e6a08457
nino.roldan <nino_roldan@yahoo.com>
2014-06-25 18:24:58 +0900

    initial commit

---
+++ nrcrm/exclient_show.php
@@ -0,0 +1,39 @@
+<?php
+  session_start();
+  $pagetitle = "New Interaction";
+?>
+<html>
+<?php
+  include('inc/header.php');
+  include('inc/sessionvalidation.php');
+  include('inc/dbhelper.php');
+?>
+<?php
+
+	$exclientid = cleaninput($_GET['id']);
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+</body>
+</html>
